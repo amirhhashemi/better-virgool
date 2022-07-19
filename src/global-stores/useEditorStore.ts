00b533764ab00266221fe09517f510d6a2d12e3f
Arman Hashemi <ahhdev@gmail.com>
2022-07-19 11:52:12 +0430

    fix: debounce local storage persistent

--- src/global-stores/useEditorStore.ts
+++ src/global-stores/useEditorStore.ts
@@ -1,5 +1,7 @@
 import create from "zustand";
-import { persist } from "zustand/middleware";
+import _debounce from "lodash.debounce";
+
+import { persist, StateStorage } from "zustand/middleware";
 
 interface EditorStore {
   contentHtml: string;
@@ -8,6 +10,18 @@ interface EditorStore {
   setContentHtml: (content: string) => void;
   clear: () => void;
 }
+
+const LocalStorage: StateStorage = {
+  getItem: async (name) => {
+    return localStorage.getItem(name) || null;
+  },
+  setItem: _debounce(async (name, value) => {
+    localStorage.setItem(name, value);
+  }, 1500),
+  removeItem: async (name) => {
+    localStorage.removeItem(name);
+  },
+};
 
 export const useEditorStore = create<EditorStore>()(
   persist(
@@ -20,6 +34,7 @@ export const useEditorStore = create<EditorStore>()(
     }),
     {
       name: "editor",
+      getStorage: () => LocalStorage,
     }
   )
 );
