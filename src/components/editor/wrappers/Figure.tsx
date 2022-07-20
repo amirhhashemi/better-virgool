604c270a07a70dc80791dec44a79539ca004ae58
Arman Hashemi <ahhdev@gmail.com>
2022-07-20 20:32:54 +0430

    fix: remove image upload simulation logic

--- src/components/editor/wrappers/Figure.tsx
+++ src/components/editor/wrappers/Figure.tsx
@@ -1,37 +1,11 @@
-import cn from "clsx";
-
-import { useState, useEffect, useRef } from "react";
 import { NodeViewWrapper, NodeViewProps, NodeViewContent } from "@tiptap/react";
 
 import { X } from "../../../icons";
 
 export const FigureWrapper = ({ node, deleteNode }: NodeViewProps) => {
-  const uploaded = useRef(false);
-  const [uploading, setUploading] = useState(false);
-
-  useEffect(() => {
-    let timer: number;
-
-    if (!uploaded.current) {
-      setUploading(true);
-      timer = setTimeout(() => {
-        uploaded.current = true;
-        setUploading(false);
-      }, 1500);
-    }
-
-    return () => clearTimeout(timer);
-  }, [uploading]);
-
   return (
     <NodeViewWrapper as="figure" className="relative" data-type="figure">
-      {node.attrs.src && (
-        <img
-          src={node.attrs.src}
-          alt="Image"
-          className={cn(uploading && "blur-sm")}
-        />
-      )}
+      {node.attrs.src && <img src={node.attrs.src} alt="Image" />}
       <NodeViewContent as="figcaption" className="text-center" dir="auto" />
 
       <X
